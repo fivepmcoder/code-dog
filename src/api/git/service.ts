d0d27a7c91ba6524404e954c8db0638cbe9e40a3
Zero <mengxiangyu996@qq.com>
2026-02-05 08:52:01 +0800

    feat: service

---
+++ src/api/git/service.ts
@@ -0,0 +1,42 @@
+import type { RepoList } from "@/definition/git/repo";
+
+/**
+ * 代码服务接口
+ */
+export interface CodeService {
+    /**
+     * 代码库列表
+     * @returns 代码库列表
+     */
+    repoList: () => Promise<RepoList[]>;
+
+    /**
+     * 代码库详情
+     * @returns 代码库详情
+     */
+    repoDetail: () => Promise<unknown>;
+
+    /**
+     * 代码库分支列表
+     * @returns 代码库分支列表
+     */
+    repoBranchList: () => Promise<unknown>;
+
+    /**
+     * 代码库成员列表
+     * @returns 代码库成员列表
+     */
+    repoMemberList: () => Promise<unknown>;
+
+    /**
+     * 代码库成员详情
+     * @returns 代码库成员详情
+     */
+    repoMemberDetail: () => Promise<unknown>;
+
+    /**
+     * 代码库提交列表
+     * @returns 代码库提交列表
+     */
+    repoCommitList: () => Promise<unknown>;
+}
