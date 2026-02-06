e8994bddfa4fa26dbc7009d7c6cf5a9fcf6fbf89
Zero <mengxiangyu996@qq.com>
2026-02-06 09:13:41 +0800

    style: layout

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="h-full flex justify-center items-center p-4 text-main">
-        <div class="bg-card w-2/4 min-w-36 p-8 rounded-lg shadow-xl">
-            <h2 class="text-2xl font-bold text-center border-b pb-10 mb-6">Git Config</h2>
+    <div class="text-main flex h-full items-center justify-center">
+        <div class="bg-card w-2/4 min-w-36 rounded-lg p-8 shadow-xl">
+            <h2 class="mb-6 border-b pb-10 text-center text-2xl font-bold">Git Config</h2>
             <form class="flex flex-col gap-6">
                 <div>
-                    <label for="name" class="block text-sm font-bold pb-1.5"
+                    <label for="name" class="block pb-1.5 text-sm font-bold"
                         >Personal Access Token *</label
                     >
                     <input
                         :type="showToken ? 'text' : 'password'"
                         id="name"
-                        class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent transition duration-200 caret-primary"
+                        class="focus:ring-primary caret-primary w-full rounded-lg border border-gray-300 px-4 py-2 transition duration-200 focus:border-transparent focus:ring-2 focus:outline-none"
                     />
                     <input
                         class="cursor-pointer"
@@ -19,13 +19,13 @@
                         @change="!showToken"
                     />
                     显示令牌
-                    <p class="opacity-60 text-xs">
+                    <p class="text-xs opacity-60">
                         在云效控制台获取：个人设置 -> 访问令牌 -> 新建令牌
                     </p>
                 </div>
 
                 <button
-                    class="cursor-pointer w-full max-w-xs mx-auto bg-primary text-white font-bold py-2 px-4 rounded-full transform duration-500 hover:-translate-y-1 hover:shadow-2xl"
+                    class="bg-primary mx-auto w-full max-w-xs transform cursor-pointer rounded-full px-4 py-2 font-bold text-white duration-500 hover:-translate-y-1 hover:shadow-2xl"
                 >
                     下一步
                 </button>
